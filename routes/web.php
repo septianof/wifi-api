5c42f51ee7ec9f76f58ba7cd786aedd8976a5b59
septianof <tiokovo03@gmail.com>
2025-07-15 11:44:13 +0700

    menghapus views pelanggan karena sudah tidak diperlukan

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pelanggan.index');
-});
-
-// View untuk index pelanggan
-Route::view('/pelanggan', 'pelanggan.index');
-
-// View untuk tambah pelanggan
-Route::view('/pelanggan/create', 'pelanggan.create');
-
-// View untuk edit pelanggan
-Route::view('/pelanggan/edit', 'pelanggan.edit');
+Route::redirect('/', '/admin/login');
